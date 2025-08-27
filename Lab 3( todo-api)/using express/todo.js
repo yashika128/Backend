9f1d1fdb8d21636bd431ded0bf9acaa2de182e61
yashika garg <yashikagarg@yashikas-MacBook-Air.local>
2025-08-27 16:17:27 +0530

    Lab 3

---
+++ Lab 3( todo-api)/using express/todo.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+let todos = [
+  { id: 1, task: "Learn HTTP methods" },
+  { id: 2, task: "Build first REST API" }
+];
+
+// GET - fetch all todos
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+// POST - add a new todo
+app.post("/todos", (req, res) => {
+  const newTodo = { id: todos.length + 1, task: req.body.task };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+
+// DELETE - remove a todo
+app.delete("/todos/:id", (req, res) => {
+  const {id} = req.params;
+  todos = todos.filter(t => t.id !== id);
+  res.json({ message: "Todo deleted" });
+});
+
+app.listen(8000, () => console.log("Server running on http://localhost:8000"));
---
+++ Lab 3( todo-api)/using http/todo2.js
@@ -0,0 +1,37 @@
+import http from "http";
+
+let todos = [
+  { id: 1, task: "Learn HTTP methods" },
+  { id: 2, task: "Build REST API with http module" }
+];
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+
+  if (req.method === "GET" && req.url === "/todos") {
+    res.writeHead(200);
+    res.end(JSON.stringify(todos));
+  } 
+  
+  else if (req.method === "POST" && req.url === "/todos") {
+    let body = "";
+    req.on("data", chunk => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      const newTodo = { id: todos.length + 1, task: JSON.parse(body).task };
+      todos.push(newTodo);
+      res.writeHead(201);
+      res.end(JSON.stringify(newTodo));
+    });
+  } 
+  
+  else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: "Not Found" }));
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
